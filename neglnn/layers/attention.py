8477d6e235d7c52970749cce430e4bddad3a3c78
Omar Aflak <aflakomar@gmail.com>
2023-03-26 11:29:03 +0100

    attention layer extend model

--- neglnn/layers/attention.py
+++ neglnn/layers/attention.py
@@ -1,5 +1,5 @@
 import numpy as np
-from neglnn.layers.layer import Layer
+from neglnn.layers.model import Model
 from neglnn.layers.dense import Dense
 from neglnn.layers.transpose import Transpose
 from neglnn.layers.dot import Dot
@@ -8,12 +8,10 @@ from neglnn.activations.linear import Linear
 from neglnn.activations.softmax_row import SoftmaxRow
 from neglnn.network.network import Network
 from neglnn.network.graph import Graph
-from neglnn.utils.types import Array, InputKey, Shape2
+from neglnn.utils.types import Shape2
 
-class Attention(Layer):
+class Attention(Model):
     def __init__(self, input_shape: Shape2, output_shape: Shape2, key_dimensions: int, **kwargs):
-        super().__init__(input_shape=input_shape, output_shape=output_shape, trainable=True, **kwargs)
-
         input_size = input_shape[0]
         key_shape = (input_size, key_dimensions)
 
@@ -38,22 +36,6 @@ class Attention(Layer):
         graph.connect(dot_qkt_scaled_layer, softmax_layer)
         graph.connect(softmax_layer, dot_v_layer, 'a')
         graph.connect(v_layer, dot_v_layer, 'b')
-        self.network = Network(graph, input_layer, dot_v_layer)
+        network = Network(graph, input_layer, dot_v_layer)
 
-    def initialize_parameters(self):
-        pass
-
-    def parameters(self) -> list[Array]:
-        return []
-
-    def parameters_gradient(self, output_gradient: Array) -> list[Array]:
-        return []
-
-    def forward(self, inputs: dict[InputKey, Array]) -> Array:
-        return self.network.run(inputs[Graph.INPUT])
-
-    def input_gradient(self, output_gradient: Array) -> dict[InputKey, Array]:
-        return {Graph.INPUT: self.network.record_gradient(output_gradient, optimize=False)}
-
-    def optimize(self):
-        self.network.optimize()
+        super().__init__(network, input_shape=input_shape, output_shape=output_shape)
